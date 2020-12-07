1a9c543e2ee1d137cd99cb0cd24d260bcafeaccb
Литвиненко Юрий <litvinenko-don@ukr.net>
2020-12-07 14:06:54 +0300

    added Proxy, but not working

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ function Person(name, age, gender) {
   //это функция-конструктор. Она конструирует объекты. H-р, новых персон
   this.name = name,
     this.age = age,
-    this.gender = gender,
-    Object.defineProperty(this, "gender", { writable: false }),
+    this._gender = gender,
+    Object.defineProperty(this, "_gender", { writable: false }), // изменить дескриптор
     this.children = [],
 
     this.printInfo = function () {
@@ -15,7 +15,7 @@ function Person(name, age, gender) {
       console.log(`данные по: ${this.name}`);
       console.log(`name = ${this.name}`);
       console.log(`age = ${this.age}`);
-      console.log(`gender = ${this.gender}`);
+      console.log(`gender = ${this._gender}`);
       console.log(`*******************`);
     },
 
@@ -38,38 +38,67 @@ function Person(name, age, gender) {
     };
 }
 
+
 /*********************************/
 /**создаем новых людей*/
 let ivan = new Person('Ivan', 25, 'men'),
   alex = new Person('Alex', 22, 'men'),
   helen = new Person('Helen', 28, 'women');
 
-
+/*********************************/
 /**смотрим, что люди создались */
 console.log(ivan);
 console.log(alex);
 console.log(helen);
 
-
+/*********************************/
 /*тест методов на Иване*/
 ivan.printInfo(); // вывести name, age и gender персонажа
 ivan.addChildren(5); //добавить n детей в массив children у персонажа
 ivan.printChildren(); // вывести детей персонажа
 
-
+/*********************************/
 /*тест метода .incrementAge() на Иване*/
 console.log(`Возраст Ивана, старое значение = ${ivan.age}`);
 ivan.incrementAge(); //добавить n детей в массив children
 console.log(`Возраст Ивана, новое значение = ${ivan.age}`);
 
+/*********************************/
 /*тест дескрипторы gender на Иване*/
-let descriptorIvan = Object.getOwnPropertyDescriptor(ivan, 'gender'); // посмотреть дескрипторы для gender
+let descriptorIvan = Object.getOwnPropertyDescriptor(ivan, '_gender'); // посмотреть дескрипторы для gender
 console.log(`*******************`);
-console.log(`Дескриптор для ivan.gender:`);
+console.log(`Дескриптор для ivan._gender:`);
 console.log(descriptorIvan);
 
+/*********************************/
 /*тест изменения ivan.age*/
 ivan.age = 50;
 console.log(`*******************`);
 console.log(`ivan.age = ${ivan.age}`); // оно изменилось...
+console.log(`оно изменилось...`);
 
+ivan = new Proxy(ivan, {
+  set(target, prop, val) { // перехватываем запись свойства
+    if (prop.startsWith('_')) {
+      throw new Error("Отказано в доступе на запись");
+    } else {
+      target[prop] = val;
+      return true;
+    }
+  },
+  deleteProperty(target, prop) { // перехватываем удаление свойства
+    if (prop.startsWith('_')) {
+      throw new Error("Отказано в доступе на удаление");
+    } else {
+      delete target[prop];
+      return true;
+    }
+  }
+});
+
+ivan.age = 75;
+console.log(`*******************`);
+console.log(`ivan.age = ${ivan.age}`); // оно ОПЯТЬ изменилось...
+console.log(`оно ОПЯТЬ изменилось...`);
+console.log(`*******************`);
+console.log(`ЭТО ФИАСКО, БРАТАН...`);
---
+++ script2.js
@@ -0,0 +1,55 @@
+'use strict';
+
+function User(name, age) {
+  this.name = name,
+    this._age = age
+}
+
+/**создаем новых людей*/
+let user = new User('Ivan', '25');
+
+user = new Proxy(user, {
+  set(target, prop, val) { // перехватываем запись свойства
+    if (prop.startsWith('_')) {
+      throw new Error("Отказано в доступе на запись");
+    } else {
+      target[prop] = val;
+      return true;
+    }
+  },
+  deleteProperty(target, prop) { // перехватываем удаление свойства
+    if (prop.startsWith('_')) {
+      throw new Error("Отказано в доступе на удаление");
+    } else {
+      delete target[prop];
+      return true;
+    }
+  }
+});
+
+
+/**смотрим, что люди создались */
+console.log(user);
+
+/*тест изменения user.age*/
+user.age = 50;
+console.log(`*******************`);
+console.log(`ivan.age = ${user.age}`); // оно изменилось...
+
+
+
+
+
+
+/****************** */
+/****************** */
+// "set" не позволяет записать _password
+try {
+  user._password = "test"; // Error: Отказано в доступе
+} catch (e) { console.log(e.message); }
+
+// "deleteProperty" не позволяет удалить _password
+try {
+  delete user._password; // Error: Отказано в доступе
+} catch (e) { console.log(e.message); }
+
